28348a86243bf22ce394c883f5100a8812744760
yasmimlb <161394259+yasmimlb@users.noreply.github.com>
2024-08-21 12:23:17 -0300

    Create AT1_ManipulacaoDeVariaveisSimples.kt
    praticar a criação e manipulação de variáveis, realizando operações básicas em Kotlin, decidindo por conta própria se usarão val ou var.

---
+++ AT1_ManipulacaoDeVariaveisSimples.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val idadePessoa1 = 20
+    val alturaPessoa1 = 1.73
+    val nomePessoa1 = "Yasmim Dias"
+    val isEstudantePessoa1 = true
+    val idadePessoa2 = 45
+    val nomePessoa2 = Lene
+    val somaIdades = idadePessoa1 + idadePessoa2
+    val alturaDobrada = alturaPessoa1 * 2
+    val saudacao = "Oie, $nomePessoa1 e $nomePessoa2!!!!"
+
+    println("Idade: $idadePessoa1")
+    println("Altura: $alturaPessoa1 metros")
+    println("Nome: $nomePessoa1")
+    println("É estudante: $isEstudantePessoa1")
+    println("Soma das idades: $somaIdades")
+    println("Altura dobrada: $alturaDobrada metros")
+    println(saudacao)
+}
